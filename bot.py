b0e60117598e68e4fd12c67ead1b56e8c2c3ccc9
WhiteRicex <jacob_kotro@hotmail.com>
2025-12-03 08:46:36 +1100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,32 @@
+import os
 import requests
 import time
+import json
 
 from discord import SyncWebhook
 
-with open("settings.txt", "r") as file:
-    author = file.read()
+from atproto import Client
+from dotenv import load_dotenv
+
+load_dotenv()
+client = Client()
+client.login(os.getenv("BLUESKY_USERNAME"), os.getenv("BLUESKY_PASSWORD"))
+
+with open("settings.json", "r") as file:
+    settingsData = json.load(file)
+    author = settingsData["author"]
+    webhook_url = settingsData["webhook"]
     
 url = "https://public.api.bsky.app/"
 
-fileName = "contentID.txt"
-
-webhook_url = "https://discord.com/api/webhooks/1445507968828313710/La-LnmcPd1JC_gnO4YijbQNwhTQISUBBmIc03b3bi3uV3M9dDAdjzkooDq0859v7cxT_"
+contentIDFile = "contentID.json"
 
 starttime = time.monotonic()
 
 def main():
-    with open(fileName, "r") as file:
-        cacheID = file.read()
+    with open(contentIDFile, "r") as file:
+        contentIDdata = json.load(file)
+        cacheID = contentIDdata["cid"]
         print(cacheID)
 
     getAuthorFeed = requests.get(url + "/xrpc/app.bsky.feed.getAuthorFeed", params={"actor": author, "limit": "1", "filter": "posts_with_media"})
@@ -25,21 +35,21 @@ def main():
     if(getAuthorFeed.status_code == 200):
         print("get author feed ok!")
 
-        data = getAuthorFeed.json()
+        authorFeedData = getAuthorFeed.json()
 
-        contentID = data["feed"][0]["post"]["cid"]
-        image = data["feed"][0]["post"]["embed"]["images"][0]["fullsize"]
+        contentID = authorFeedData["feed"][0]["post"]["cid"]
+        image = authorFeedData["feed"][0]["post"]["embed"]["images"][0]["fullsize"]
         print("contentID", contentID)
 
         if(cacheID != contentID):
-            with open(fileName, "w") as file:
+            with open(contentIDFile, "w") as file:
                 print("no match! posting new")
+
+                contentIDdata["cid"] = contentID
+                json.dump(contentIDdata, file)
 
                 syncWebhook = SyncWebhook.from_url(webhook_url)
                 syncWebhook.send(image)
-
-                file.truncate(0)
-                file.write(contentID)
 
 while(True):
     main()
